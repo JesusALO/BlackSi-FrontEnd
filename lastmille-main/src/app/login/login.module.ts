db1d77015f688a4eb9115106095d9ae703d1dc91
MarsClaim <Luivil777@gmail.com>
2023-06-16 19:13:47 -0500

    BackgroundSwitching

--- lastmille-main/src/app/login/login.module.ts
+++ lastmille-main/src/app/login/login.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { LoginPageRoutingModule } from './login-routing.module';
 
 import { LoginPage } from './login.page';
+import Swiper from 'swiper';
 
 @NgModule({
   imports: [
@@ -15,6 +16,7 @@ import { LoginPage } from './login.page';
     IonicModule,
     LoginPageRoutingModule
   ],
-  declarations: [LoginPage]
+  declarations: [LoginPage],
+  providers:[Swiper]
 })
 export class LoginPageModule {}
--- lastmille-main/src/app/login/login.page.ts
+++ lastmille-main/src/app/login/login.page.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import Swiper, { Pagination } from 'swiper';
+
+Swiper.use([Pagination]);
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage implements OnInit, AfterViewInit {
+  slides = [
+    '../../assets/black-white.jpg',
+    '../../assets/black-white2.jpg',
+    '../../assets/black-white3.jpg',
+  ];
 
-  constructor() { }
+  swiper: Swiper =  new Swiper('.swiper-container', {
+	slidesPerView: 1,
+	loop: true,
+	autoplay: {
+	  delay: 3000,
+	},
+  });
 
-  ngOnInit() {
+  constructor(swiper:Swiper){
+	//swiper: Swiper; // Add "!" to indicate that it will be initialized later
+
   }
 
+
+  ngOnInit() {}
+
+  
+  ngAfterViewInit() {
+
+  }
 }
