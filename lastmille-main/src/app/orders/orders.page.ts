db01e79b2786738c581612a980d940047a31a7da
JesusALO <71664947+JesusALO@users.noreply.github.com>
2023-07-12 17:54:39 -0700

    orders

--- lastmille-main/src/app/orders/orders.page.ts
+++ lastmille-main/src/app/orders/orders.page.ts
@@ -3,6 +3,10 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { NavController } from '@ionic/angular';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
 
 @Component({
   selector: 'app-orders',
@@ -10,44 +14,45 @@ import { Router } from '@angular/router';
   styleUrls: ['./orders.page.scss'],
 })
 export class OrdersPage implements OnInit {
-  jsonData: any[] = [
-    { producto: 'Pantalón', talla: 'M', precio: 120 },
-    { producto: 'Pantalón', talla: 'M', precio: 120 },
-    { producto: 'Pantalón', talla: 'M', precio: 120 },
-    { producto: 'Pantalón', talla: 'M', precio: 120 },
-    { producto: 'Pantalón', talla: 'M', precio: 120 },
-    { producto: 'Pantalón', talla: 'M', precio: 120 },
-    { producto: 'Pantalón', talla: 'M', precio: 120 },
-    { producto: 'Pantalón', talla: 'M', precio: 120 },
-    { producto: 'Pantalón', talla: 'M', precio: 120 },
-    { producto: 'Pantalón', talla: 'M', precio: 120 },
-    { producto: 'Pantalón', talla: 'M', precio: 120 },
-    { producto: 'Pantalón', talla: 'M', precio: 120 },
-    { producto: 'Pantalón', talla: 'M', precio: 120 },
-    { producto: 'Pantalón', talla: 'M', precio: 120 },
-    { producto: 'Pantalón', talla: 'M', precio: 120 },
-    { producto: 'Pantalón', talla: 'M', precio: 120 },
-    { producto: 'Pantalón', talla: 'M', precio: 120 },
-    { producto: 'Pantalón', talla: 'M', precio: 120 },
-    { producto: 'Pantalón', talla: 'M', precio: 120 },
-    { producto: 'Pantalón', talla: 'M', precio: 120 }
-  ];
+  jsonData: any[] = [];
+  orders: any[] = [];
+
+  fetchOrders() {
+    console.log();
+    this.http.get('<https://api.tiendanube.com/v1/3378902/orders?fields=id,contact_name,contact_phone,customer>')
+      .pipe(
+        catchError((error) => {
+          console.error('Error fetching orders:', error);
+          return throwError(error);
+        })
+      )
+      .subscribe((data: any) => {
+        this.orders = data;
+      });
+  }
+  
   
   calculateTotalPrice(){
     let totalPrice = 0;
-    for (let item of this.jsonData){
-      totalPrice += item.price;
+    for (let order of this.jsonData){
+      totalPrice += order.price;
     }
     return totalPrice;
   }
 
-  constructor(private authService: AuthService, private navCtrl: NavController, private router: Router) { }
+  constructor(
+    private authService: AuthService, 
+    private navCtrl: NavController, 
+    private router: Router,
+    private http: HttpClient
+    ) { }
 
   confirm() {
     this.router.navigate(['/confirmed-orders']);
   }
 
   ngOnInit(): void {
+    this.fetchOrders();
   }
 
 }
